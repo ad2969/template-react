7e804b49c0ea69db1d68e352c9afa982441c7e16
ad2969 <clarenceadrian@alumni.ubc.ca>
2021-05-13 22:19:07 -0700

    temp

---
+++ config-overrides.js
@@ -0,0 +1,21 @@
+/* config-overrides.js */
+
+module.exports = (config) => {
+  // add sass loader
+  // allow global vars (https://stackoverflow.com/questions/44345881/using-webpack-to-prepend-variables-for-scss)
+  // js sharing (https://til.hashrocket.com/posts/sxbrscjuqu-share-scss-variables-with-javascript)
+  config.module.rules.push({
+    test: /\.scss$/,
+    use: [
+      {
+        loader: "sass-loader",
+        query: {
+          sourceMap: false,
+          prependData: "@import \"src/styles/app.scss\";",
+        },
+      },
+    ],
+  });
+  
+  return config;
+};
