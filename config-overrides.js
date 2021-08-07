21c24c9f8b51bfbf80751bc18106fcc350b947b1
Clarence Adrian <clarenceadrian@alumni.ubc.ca>
2021-08-07 16:34:33 -0700

    fix scss to js imports and global scss vars

--- config-overrides.js
+++ config-overrides.js
@@ -5,9 +5,10 @@ module.exports = (config) => {
     // allow global vars (https://stackoverflow.com/questions/44345881/using-webpack-to-prepend-variables-for-scss)
     // js sharing (https://til.hashrocket.com/posts/sxbrscjuqu-share-scss-variables-with-javascript)
     config.module.rules.push({
-    // include all files except those located in folder `styles/*`
-    // test: /^((?!styles\/).)*\.scss$/,
-        test: /\.module\.scss$/,
+        // test: /^((?!styles\/).)*\.scss$/,
+        // test: /\.module\.scss$/,
+        // include all files except those with leading underscore such as */_main.scss`
+        test: /^((?!_).)*\.scss$/,
         use: [
             {
                 loader: "sass-loader",
