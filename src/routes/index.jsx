154382bf4b627a006d728df78c1a2da6feb85a46
Clarence Adrian <clarenceadrian@alumni.ubc.ca>
2021-08-13 03:05:08 -0700

    remove double header

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { Route, BrowserRouter, Switch } from "react-router-dom";
 
-import Header from "../components/header";
-
 // import pages
 import Home from "./Home";
 import NotFound from "./NotFound";
@@ -10,7 +8,6 @@ import NotFound from "./NotFound";
 const Routes = () => {
     return (
         <BrowserRouter>
-            <Header/>
             <Switch>
                 {/* PUBLIC ROUTES */}
                 <Route exact path="/" component={Home} />
