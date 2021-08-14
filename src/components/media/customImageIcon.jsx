a0abf6b72bbba1e1788fdee7999ad3617ffe6119
Clarence Adrian <clarenceadrian@alumni.ubc.ca>
2021-08-13 18:38:47 -0700

    fix new layout mixin

--- src/components/media/customImageIcon.jsx
+++ src/components/media/customImageIcon.jsx
@@ -2,11 +2,7 @@ import React from "react";
 
 import "./styles.scss";
 
-interface Props extends React.HTMLAttributes<HTMLDivElement> {
-    img: string
-}
-
-const CustomImageIcon: React.FunctionComponent<Props> = ({ img }) => {
+const CustomImageIcon = ({ img }) => {
     return (
         <div className="custom-icon" >
             <img className="custom-icon__img" src={img} alt="icon" />
